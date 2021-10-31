05c45a60506a9bea1b2a8908275ca08241f6d74c
Soma_macbookAir <m11232mh@gmail.com>
2021-11-01 03:02:48 +0900

    fix config/setting/local.py
    add database_setting

--- config/setting/local.py
+++ config/setting/local.py
@@ -4,3 +4,10 @@ from .production import *
 SECRET_KEY = '*'
 DEBUG = True
 ALLOWED_HOSTS = ['*']
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
