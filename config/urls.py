c90bb16b1a9c36d9b3aa84cc39ad7c96028d9bee
Soma_macbookAir <m11232mh@gmail.com>
2021-11-01 01:51:53 +0900

    add django_taskapp
    Divided urls.py

--- config/urls.py
+++ config/urls.py
@@ -15,9 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from django.views.generic import TemplateView
+from django.urls import include
 
 urlpatterns = [
-	path('', TemplateView.as_view(template_name='home.html')), 
+	path('', include('django_taskapp.urls')), 
 	path('admin/', admin.site.urls),
 ]
