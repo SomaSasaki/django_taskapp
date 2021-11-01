3f81a32acc492e329161fef33e06c8955c76edf5
Soma_macbookAir <m11232mh@gmail.com>
2021-11-01 13:08:06 +0900

    add test_view.py
    simple get_response test

---
+++ django_taskapp/tests/test_views.py
@@ -0,0 +1,9 @@
+from django.test import Client
+from django.test import TestCase
+
+class TestHomeView(TestCase):
+    def test_get_home_view(self):
+        c=Client()
+        response = c.get('')
+        self.assertEqual(response.status_code,200)
+        self.assertTrue('hello' in str(response.content))
