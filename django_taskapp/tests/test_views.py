0389e3802424b568be99b186f15cfbedbc6e7b61
Soma_macbookAir <m11232mh@gmail.com>
2021-11-02 20:10:10 +0900

    fix test_veiws.py
    install pycharm

--- django_taskapp/tests/test_views.py
+++ django_taskapp/tests/test_views.py
@@ -3,7 +3,7 @@ from django.test import TestCase
 
 class TestHomeView(TestCase):
     def test_get_home_view(self):
-        c=Client()
+        c = Client()
         response = c.get('')
-        self.assertEqual(response.status_code,200)
+        self.assertEqual(response.status_code, 200)
         self.assertTrue('hello' in str(response.content))
