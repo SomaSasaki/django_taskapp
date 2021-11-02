bd59169a4ec44a99d365eb796cdd24b7b89e015b
Soma_macbookAir <m11232mh@gmail.com>
2021-11-02 23:10:30 +0900

    add DemoModel

--- django_taskapp/models.py
+++ django_taskapp/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class DemoModel(models.Model):
+    title = models.CharField(max_length=255)
+    number = models.IntegerField(null=True)
+
+#demo = DemoModel(title="aaa", number=1)
+#demo = DemoModel(title="aaa", number=2)
+#demo = DemoModel(title="aaa", number=3)
+#demo.save()
--- django_taskapp/views.py
+++ django_taskapp/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
 from django.views.generic.base import TemplateView
+from django.views.generic import ListView
+from .models import DemoModel
 
-class HomeView(TemplateView):
+class HomeView(ListView):
 	template_name = "home.html"
+	model = DemoModel
+	context_object_name = 'demomodels'
