9b9d58dd907673da4da3edcc8663a749de1d2504
Soma_macbookAir <m11232mh@gmail.com>
2021-11-04 00:30:07 +0900

    fix models.py
    create demoData

--- django_taskapp/models.py
+++ django_taskapp/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+import datetime
 
 class DemoSchedule(models.Model):
     title = models.CharField(max_length=255)
@@ -6,3 +7,8 @@ class DemoSchedule(models.Model):
     date = models.DateTimeField()
     delta = models.IntegerField()
     together = models.CharField(max_length=255)
+
+# demo = DemoSchedule(title='か', field='き', \
+#                     date=datetime.datetime(year=2022, month=2, day=1, hour=12), \
+#                     delta=2, together="けんじ")
+# demo.save()
