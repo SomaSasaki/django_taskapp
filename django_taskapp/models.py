f09e6a69fa36ec4d7c5f7f603eeedd57142f2d83
Soma_macbookAir <m11232mh@gmail.com>
2021-11-04 23:30:17 +0900

    fix models.py

--- django_taskapp/models.py
+++ django_taskapp/models.py
@@ -1,13 +1,15 @@
 from django.db import models
 import datetime
 
-class DemoSchedule(models.Model):
+
+class DemoScheduleTable(models.Model):
     title = models.CharField(max_length=255)
     field = models.CharField(max_length=255)
     date = models.DateTimeField()
     delta = models.IntegerField()
-    together = models.CharField(max_length=255)
-# demo = DemoSchedule(title='あいうえおかきくけこさしすせそたちつてと', field='お台場', \
-#                     date=datetime.datetime(year=2022, month=12, day=10, hour=12), \
-#                     delta=4, together="けんじ,ささき,たろう")
+
+
+# demo = DemoScheduleTable(title='クラス会', field='お台場', \
+#                          date=datetime.datetime(year=2022, month=12, day=10, hour=12), \
+#                          delta=4)
 # demo.save()
--- django_taskapp/views.py
+++ django_taskapp/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render
 from django.views.generic.base import TemplateView
 from django.views.generic import ListView
-from .models import DemoSchedule
+from .models import DemoScheduleTable
 
 class HomeView(ListView):
 	template_name = "home.html"
-	model = DemoSchedule
+	model = DemoScheduleTable
 	context_object_name = 'demoSchedule'
