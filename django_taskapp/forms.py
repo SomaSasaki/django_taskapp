62cbb5e95cc8ecc638738a2bcd5c0b850f8b0c1b
Soma_macbookAir <m11232mh@gmail.com>
2021-11-06 01:12:07 +0900

    fix registration.html
    calender Input

--- django_taskapp/forms.py
+++ django_taskapp/forms.py
@@ -1,8 +1,18 @@
 from django import forms
 from .models import DemoScheduleTable
+import bootstrap_datepicker_plus as datetimepicker
 
 
 class RegistrationForm(forms.ModelForm):
     class Meta:
         model = DemoScheduleTable
         fields = '__all__'
+        widgets = {
+            'date': datetimepicker.DateTimePickerInput(
+                format='%Y-%m-%d %H:%M',
+                options={
+                    'locale': 'ja',
+                    'dayViewHeaderFormat': 'YYYY年 MMMM',
+                }
+            ),
+        }
--- django_taskapp/models.py
+++ django_taskapp/models.py
@@ -1,5 +1,4 @@
 from django.db import models
-import datetime
 
 
 class DemoScheduleTable(models.Model):
