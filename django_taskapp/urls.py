3792a288253a673fb448ca9553e9d94063c6da17
Soma_macbookAir <m11232mh@gmail.com>
2021-11-05 21:59:29 +0900

    fix urls.py
    linked home & registration

--- django_taskapp/urls.py
+++ django_taskapp/urls.py
@@ -3,6 +3,6 @@ from django_taskapp.views import HomeView
 from django_taskapp.views import RegistrationTaskView
 
 urlpatterns = [
-    path('', HomeView.as_view()),
-    path('register/', RegistrationTaskView.as_view()),
+    path('', HomeView.as_view(), name='home'),
+    path('register/', RegistrationTaskView.as_view(), name='registration'),
 ]
