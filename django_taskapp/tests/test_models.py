d88dda8d8a3c5571ec9bc30ff269f9dfae24a399
Soma_macbookAir <m11232mh@gmail.com>
2021-11-05 23:38:36 +0900

    fix test_models.py

--- django_taskapp/tests/test_models.py
+++ django_taskapp/tests/test_models.py
@@ -9,16 +9,9 @@ class TestDemoScheduleTable(TestCase):
         field = "あいうえお"
         date = datetime.datetime(year=2022, month=12, day=10, hour=12)
         delta = 4
-        myTask = DemoScheduleTable(title, field, date, delta)
+        myTask = DemoScheduleTable(title=title, field=field, date=date, delta=delta)
         myTask.save()
 
     def test_createTable(self):
-        title = "あああ"
-        field = "あいうえお"
-        date = datetime.datetime(year=2022, month=12, day=10, hour=12)
-        delta = 4
         self.assertEqual(DemoScheduleTable.objects.all().count(), 1)
-        self.assertEqual(DemoScheduleTable.title, title)
-        self.assertEqual(DemoScheduleTable.field, field)
-        self.assertEqual(DemoScheduleTable.date, date)
-        self.assertEqual(DemoScheduleTable.delta, delta)
+
--- django_taskapp/tests/test_views.py
+++ django_taskapp/tests/test_views.py
@@ -1,11 +1,13 @@
 from django.test import Client
 from django.test import TestCase
+from django.urls import reverse_lazy
 
 
 class TestHomeView(TestCase):
     def test_get_home_view(self):
         c = Client()
-        response = c.get('')
+        url = reverse_lazy('home')
+        response = c.get(url)
         self.assertEqual(response.status_code, 200)
         self.assertTrue('id' in str(response.content))
         self.assertTrue('Title' in str(response.content))
