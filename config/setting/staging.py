e0f74acb1f4b3ebff1c9e9cf956ed563013ec17e
Soma Sasaki <m11232mh@gmail.com>
2021-11-07 17:14:32 +0900

    fix models.py

--- config/setting/staging.py
+++ config/setting/staging.py
@@ -127,5 +127,6 @@ STATICFILES_DIRS = [
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 BOOTSTRAP4 = {
+    'set_placeholder': False,
     'include_jquery': True,
 }
--- django_taskapp/models.py
+++ django_taskapp/models.py
@@ -3,9 +3,9 @@ import datetime
 
 
 class ScheduleModel(models.Model):
-    summary = models.CharField(max_length=20)
-    place = models.CharField(max_length=20)
-    date = models.DateField()
-    time = models.TimeField(default=datetime.time(hour=12, minute=0))
-    detail = models.TextField(blank=True)
+    summary = models.CharField("タイトル　※必須",max_length=20)
+    date = models.DateField("日付　※必須")
+    place = models.CharField("場所",max_length=20,blank=True)
+    time = models.TimeField("時間")
+    detail = models.TextField("詳細",blank=True)
     author = models.CharField(max_length=20, default="あなた")
