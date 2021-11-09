4547003ea1665ce6b79cf23767fd173c015c6b40
Soma Sasaki <m11232mh@gmail.com>
2021-11-10 07:35:40 +0900

    fix test_views.py

--- django_taskapp/tests/test_views.py
+++ django_taskapp/tests/test_views.py
@@ -7,21 +7,21 @@ from django_taskapp.models import ScheduleModel
 class TestHomeView(TestCase):
     def test_get_home_view(self):
         c = Client()
-        url = reverse_lazy('home')
-        response = c.get(url)
+        response = c.get(reverse_lazy('home'))
         self.assertEqual(response.status_code, 200)
 
 
 class TestPostSchedule(TestCase):
     def test_status_code(self):
         c = Client()
-        url = reverse_lazy("registration")
-        response = c.get(url)
+        response = c.get(reverse_lazy("registration"))
         self.assertEqual(response.status_code, 200)
 
     def test_post_registration(self):
         c = Client()
-        url = reverse_lazy("registration")
-        response = c.post(url, {'summary': 'abcd', 'date': '2021-11-25'})
+        response = c.post(reverse_lazy("registration"), {
+            'summary': 'abcd',
+            'date': '2021-11-25'
+        })
         self.assertRedirects(response, expected_url=reverse_lazy("home"), status_code=302, target_status_code=200)
         self.assertEqual(ScheduleModel.objects.all().count(), 1)
