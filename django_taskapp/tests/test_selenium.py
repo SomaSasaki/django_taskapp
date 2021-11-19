c95a6e12cf97e9ebf52e5f2e5d8e3db8ac911ff9
Soma_macbookAir <m11232mh@gmail.com>
2021-11-19 22:51:16 +0900

    fix test_selenium

--- django_taskapp/tests/test_selenium.py
+++ django_taskapp/tests/test_selenium.py
@@ -1,5 +1,6 @@
 from django.test import TestCase
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 
 
 class TestFireFox(TestCase):
@@ -10,5 +11,12 @@ class TestFireFox(TestCase):
     def test_home(self):
         driver = self.driver
         driver.get("http://localhost:8000")
+
+        ele_text = []
+        elements = driver.find_elements(By.TAG_NAME, 'a')
+        for e in elements:
+            ele_text.append(e.text)
+
         self.assertIn("Schedule Viewer", driver.title)
+        self.assertIn("HOME", ele_text)
         driver.quit()
